a52480d8a076fd45769f9273a72616943b3a604d
Robert Cowham <rcowham@perforce.com>
2022-05-17 10:26:55 +0000

    Cope with ssl: prefix in port

--- utils/del_clients.sh
+++ utils/del_clients.sh
@@ -10,7 +10,7 @@ function del_clients() {
     p4 -p $P4PORT -u bruno clients -e bruno* | cut -d" " -f2 | while read f; do p4 -p $P4PORT -u bruno client -d -f $f; done
 }
 
-p4port=$(ps ax | grep "p4d_${instance}" | grep -v grep | head -1 |perl -ne 'print "$1" if /\-p\s*([0-9.:]+)/')
+p4port=$(ps ax | grep "p4d_${instance}" | grep -v grep | grep " \-r " | head -1 |perl -ne 'print "$1" if /\-p\s*((ssl:)*[0-9.:]+)/')
 # Run for edge servers:
 # del_clients sn1-r720-a02-13:${instance}666
 # del_clients sn1-r720-a02-15:${instance}666
